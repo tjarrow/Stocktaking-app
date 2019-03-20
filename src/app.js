48840466f5cae4d276434ae482121a0e3fda08cd
tjarrow <nickita0104@gmail.com>
2019-03-20 14:36:54 +0300

    Remove excess styles import from App

--- src/app.js
+++ src/app.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import Scheme from './components/Scheme';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
-import './styles/styles.scss';
 
 ReactDOM.render(<Scheme/>, document.getElementById('app'));
